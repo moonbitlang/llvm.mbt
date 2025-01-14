41884b72e2484ba42a7d36d8a81c8340f204b4cd
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-01-14 10:34:52 +0800

    run fmt

--- src/llvm-binding.c
+++ src/llvm-binding.c
@@ -34,53 +34,54 @@ char *convert_moonbit_string_to_c_string(struct moonbit_string *ms) {
   return name;
 }
 
-void *__LLVMInt32Type(void *$0) {
+void *__llvm_int32_type(void *$0) {
   return LLVMInt32TypeInContext((LLVMContextRef)$0);
 }
 
-void *__LLVMContextCreate() { return LLVMContextCreate(); }
+void *__llvm_context_create() { return LLVMContextCreate(); }
 
-void *__LLVMModuleCreateWithNameInContext(void *$0, struct moonbit_string *$1) {
+void *__llvm_module_create_with_name_in_context(void *$0,
+                                                struct moonbit_string *$1) {
   char *name = convert_moonbit_string_to_c_string($1);
   LLVMContextRef context = (LLVMContextRef)$0;
   LLVMModuleRef module = LLVMModuleCreateWithNameInContext(name, context);
   return module;
 }
 
-void *__LLVMConstInt(void *$0, int32_t $1, int32_t $2) {
+void *__llvm_const_int(void *$0, int32_t $1, int32_t $2) {
   return LLVMConstInt((LLVMTypeRef)$0, $1, $2);
 }
 
 void *
-__LLVMFunctionType(void *$0,
-                   struct $$moonbitlang$core$builtin$Array$3c$Type$3e$ *$1,
-                   int32_t $2) {
+__llvm_function_type(void *$0,
+                     struct $$moonbitlang$core$builtin$Array$3c$Type$3e$ *$1,
+                     int32_t $2) {
   LLVMTypeRef *param_tys = (LLVMTypeRef *)$1->$0->data;
   int32_t param_cnt = $1->$1;
   return LLVMFunctionType((LLVMTypeRef)$0, param_tys, param_cnt, $2);
 }
 
-void __LLVMBuildRet(void *$0, void *$1) {
+void __llvm_build_ret(void *$0, void *$1) {
   LLVMBuildRet((LLVMBuilderRef)$0, (LLVMValueRef)$1);
 }
 
-void __LLVMPositionBuilderAtEnd(void *$0, void *$1) {
+void __llvm_position_builder_at_end(void *$0, void *$1) {
   LLVMPositionBuilderAtEnd((LLVMBuilderRef)$0, (LLVMBasicBlockRef)$1);
 }
 
-void *__LLVMCreateBuilder(void *$0) {
+void *__llvm_create_builder(void *$0) {
   return LLVMCreateBuilderInContext((LLVMContextRef)$0);
 }
 
-void *__LLVMAppendBasicBlock(void *$0, void *$1, struct moonbit_string *$2) {
+void *__llvm_append_basic_block(void *$0, void *$1, struct moonbit_string *$2) {
   char *block_name = convert_moonbit_string_to_c_string($2);
   return LLVMAppendBasicBlockInContext((LLVMContextRef)$0, (LLVMValueRef)$1,
                                        block_name);
 }
 
-void __LLVMDumpModule(void *$0) { LLVMDumpModule((LLVMModuleRef)$0); }
+void __llvm_dump_module(void *$0) { LLVMDumpModule((LLVMModuleRef)$0); }
 
-void *__LLVMAddFunction(void *$0, struct moonbit_string *$1, void *$2) {
+void *__llvm_add_function(void *$0, struct moonbit_string *$1, void *$2) {
   char *func_name = convert_moonbit_string_to_c_string($1);
   return LLVMAddFunction((LLVMModuleRef)$0, func_name, (LLVMTypeRef)$2);
 }
