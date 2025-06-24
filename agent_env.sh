66eff1bd0f6132bbe9a7ba108959768439d8a90b
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-06-24 16:53:05 +0800

    modify agent_env.sh

--- agent_env.sh
+++ agent_env.sh
@@ -1,4 +1,5 @@
 export PATH="/usr/lib/llvm-19/bin:$PATH"
+export C_INCLUDE_PATH="/usr/lib/llvm-19/include:$C_INCLUDE_PATH"
 export CC=gcc
-export CC_FLAGS="$(llvm-config-19 --cflags)"
-export CC_LINK_FLAGS="$(llvm-config-19 --ldflags --libs all) -lpthread -ldl -lm -lstdc++"
+export CC_FLAGS="$(llvm-config --cflags)"
+export CC_LINK_FLAGS="$(llvm-config --ldflags --libs all) -lpthread -ldl -lm -lstdc++"
